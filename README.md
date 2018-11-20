298e8887f2055f0aea887f81629ae3b00757e54e
Unknown <charles.sirois14@gmail.com>
2018-11-20 16:11:57 -0500

    Change to the readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-myModules
+Des modules de Python pour des gros nerd de Poly
+
+Modules : 
+
+	roots: 
+Function : 
+	(x1,x2,state) = quadRoots(a,b,c,show = False)
+	Input : 
+		a,b,c : Les constantes de la formule quadratique
+		show : To print the results or not
+	Output :
+		x1,x2 : Les deux racines
+		state : Wether the equation has a real solution or not
--- roots.py
+++ roots.py
@@ -7,19 +7,22 @@ Created on Thu Nov 15 12:05:27 2018
 import math
 
 
-def quadroots(a,b,c):
+def quadroots(a,b,c,show = False):
     d = b**2-4*a*c # discriminant
 
     if d < 0:
-        print ("This equation has no real solution")
+        if show:
+            print ("This equation has no real solution")
         return (0,0,False)
     elif d == 0:
         x = (-b+math.sqrt(b**2-4*a*c))/2*a
-        print ("This equation has one solutions: "), x
+        if(show):
+            print ("This equation has one solutions: "), x
         return (x,x,True)
     else:
         x1 = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a)
         x2 = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
-        print ("This equation has two solutions: ", x1, " or", x2)
+        if show:
+            print ("This equation has two solutions: ", x1, " or", x2)
         return (x1,x2,True)
     
